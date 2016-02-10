8c33604abd3cc1f8696e2546980fbf13ecbc7257
Cloud4strife <bryanboni74@gmail.com>
2016-02-10 23:20:04 +0100

    modification de la fonction injectClock

--- TpJs2/public_html/TheScript.js
+++ TpJs2/public_html/TheScript.js
@@ -1,9 +1,12 @@
 window.addEventListener("load",function (){
+    
     injectClock();
     
-    function injectClock(){
-        document.getElementById("clockDiv").textContent = date("hours")+ " : " + date("minutes") + " : " + date("second");  
-        setTimout(injectClock, 1000); // recall itself each second
-    }
     
+    function injectClock(){
+        var d = new Date();
+        document.getElementById("clockDiv").textContent = d.getHours() + " : " + d.getMinutes() + " : " + d.getSeconds();  
+        
+        setTimeout(injectClock(), 10000); // recall itself each second
+    }
 }); 
