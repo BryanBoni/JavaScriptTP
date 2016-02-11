7587c088f1e6af13e658d0d063ea99abffd0b5cd
Cloud4strife <bryanboni74@gmail.com>
2016-02-11 21:55:38 +0100

    adding css style and clock function in js, the clock work and display the time, adding empty function for the alarm management, no error

--- TpJs2/public_html/TheScript.js
+++ TpJs2/public_html/TheScript.js
@@ -1,12 +1,32 @@
 window.addEventListener("load",function (){
+    startTime();    
+    document.getElementById("addBtn").addEventListener('click', addAlarm);
     
-    injectClock();
-    
-    
-    function injectClock(){
-        var d = new Date();
-        document.getElementById("clockDiv").textContent = d.getHours() + " : " + d.getMinutes() + " : " + d.getSeconds();  
+    function addAlarm(){
+        var hours; // int
+        var minutes; // int
+        var alarmName; // String
+        var sounds; // Object
         
-        setTimeout(injectClock(), 10000); // recall itself each second
+        
     }
-}); 
+    
+    function startTime() {
+        var today = new Date();
+        var hours = today.getHours();
+        var minutes = today.getMinutes();
+        var seconds = today.getSeconds();
+        // add a zero in front of numbers<10
+        m = checkTime(m);
+        s = checkTime(s);
+        document.getElementById("clockDiv").innerHTML = hours + ":" + minutes + ":" + seconds;
+        var t = setTimeout(function(){ startTime() }, 500);
+    }
+
+    function checkTime(i) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    }
+});
